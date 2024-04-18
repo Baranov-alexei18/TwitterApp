ee8bc22177d5ba702a361a6da1f07ada3d24da1d
Alexei Baranov <alexei-baranov18@mail.ru>
2024-04-18 20:26:22 +0300

    feat(signup page): create page sign-up

---
+++ src/pages/SignUpPage/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SignUpPage = () => <div>Sign UP page</div>;
+
+export default SignUpPage;
---
+++ src/pages/StartPage/index.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import BackTwitter from '@/assets/img/backTwitter.png';
+import { Container } from '@/assets/style/global';
+import { BORDER_RADIUS, COLOR, FONT_SIZE } from '@/assets/style/variables';
+import GoogleLogo from '@/assets/svg/google-logo.svg';
+import TwitterLogo from '@/assets/svg/twitter-logo.svg';
+import { Footer } from '@/components/Footer';
+import { Button } from '@/components/ui-components/Button';
+import { ContentText } from '@/components/ui-components/ContentText';
+import { LinkApp } from '@/components/ui-components/Link';
+import { Title } from '@/components/ui-components/Title';
+import { START_PAGE } from '@/constants';
+import { PATH } from '@/constants/routerLinks';
+
+import { Content, Image } from './style';
+
+const HomePage = () => {
+  const {
+    TITLE,
+    SUBTITLE,
+    SIGN_UP_GOOGLE,
+    SIGN_UP_EMAIL,
+    LINKS_TO,
+    ACCOUNT,
+  } = START_PAGE;
+
+  return (
+    <>
+      <Container>
+        <Image src={BackTwitter} alt="Twitter Background" title="back twitter" />
+        <Content>
+          <img src={TwitterLogo} alt="twitter" title="twitter" height="50" width="41" />
+          <Title size="64px" weight="900">{TITLE}</Title>
+          <Title size="42px" weight="900">{SUBTITLE}</Title>
+          <Button
+            width="400px"
+            height="62px"
+            borderRadius={BORDER_RADIUS.xl}
+            fontSize={FONT_SIZE.xl}
+            borderColor={COLOR.lightGrey}
+            onClick={() => console.log(123)}
+          >
+            <img src={GoogleLogo} alt="google" title="google" height="50" width="41" />
+            <span>{SIGN_UP_GOOGLE}</span>
+          </Button>
+          <Button
+            width="400px"
+            height="62px"
+            borderRadius={BORDER_RADIUS.xl}
+            fontSize={FONT_SIZE.xl}
+            borderColor={COLOR.lightGrey}
+            onClick={() => console.log(123)}
+          >
+            {SIGN_UP_EMAIL}
+          </Button>
+          <ContentText
+            size={FONT_SIZE.xs}
+            width="375px"
+          >
+            {'By singing up you agree to the '}
+            <LinkApp>{LINKS_TO.service}</LinkApp>
+            {' and '}
+            <LinkApp>{LINKS_TO.policy}</LinkApp>
+            {', including '}
+            <LinkApp>{LINKS_TO.cookie}</LinkApp>
+          </ContentText>
+          <ContentText size={FONT_SIZE.sm}>
+            <span>{ACCOUNT}</span>
+            {' '}
+            <LinkApp to={PATH.LOG_IN_PAGE}>{LINKS_TO.login}</LinkApp>
+          </ContentText>
+        </Content>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
