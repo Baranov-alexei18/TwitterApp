608b26761c5affa587c7c48eeb6261a787e3fa4f
Alexei Baranov <alexei-baranov18@mail.ru>
2024-05-07 21:26:09 +0300

    feat(added emulator firebase): update jest config, firebase config, babel
    Create jest.setup.ts, firebase.json, .firebaserc

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,17 +1,21 @@
 import { getApp, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+export const PROJECT_ID = process.env.VITE_PROJECT_ID;
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
+  measurementId: process.env.VITE_MEASUREMENT_ID,
 };
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const firebase = initializeApp(firebaseConfig);
 export const firestore = getFirestore(firebase);
@@ -19,3 +23,15 @@ export const auth = getAuth(firebase);
 
 const firebaseApp = getApp();
 export const storage = getStorage(firebaseApp);
+
+if (isDevelopment) {
+  connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+  connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+}
+export const testEnvConfig = {
+  projectId: PROJECT_ID,
+  emulators: {
+    auth: connectAuthEmulator(auth, 'http://127.0.0.1:9099'),
+    firestore: connectFirestoreEmulator(firestore, '127.0.0.1', 8080),
+  },
+};
